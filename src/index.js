2c97282c8bc1ae1db8718c070d80b8c76c653cc3
DarioFlores <darioexequiel22@gmail.com>
2020-05-23 18:40:57 -0300

    Inicio del proyecto

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const morgan = require('morgan');
+
+const app = express()
+const port = 3000
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.use(morgan('dev'))
+
+app.use(router)
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
